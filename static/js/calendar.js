8f11e6db9e9cf902a2353ca538f45c784a010fca
Alex Recker <alex@reckerfamily.com>
2016-02-22 17:42:18 -0600

    Fixes calendar css
    media="print" was left off of the print css.  Added it back in,
    restoring the calendar to its former glory.
    
    Also removed now unnecessary custom prev/next/today buttons and moved js
    to closures for great fun.
    
    Fixes #32

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -1,26 +1,16 @@
 /*global $*/
-$(document).ready(function() {
-    $.get('/api/events/', {}, function(d){
-        $('#calendar').fullCalendar({
-            header: {
-                left: 'prev,next today',
-                center: 'title'
-            },
-            editable: false,
-            events: d,
-            eventLimit: true
+(function() {
+    $(document).ready(function() {
+        $.get('/api/events/', {}, function(d){
+            $('#calendar').fullCalendar({
+                header: {
+                    left: 'prev,next today',
+                    center: 'title'
+                },
+                editable: false,
+                events: d,
+                eventLimit: true
+            });
         });
     });
-
-    $('#calPrev').click(function() {
-        $('#calendar').fullCalendar('prev');
-    });
-
-    $('#calNext').click(function() {
-        $('#calendar').fullCalendar('next');
-    });
-
-    $('#calToday').click(function() {
-        $('#calendar').fullCalendar('today');
-    });
-});
+}());
--- static/js/site.js
+++ static/js/site.js
@@ -1,18 +1,21 @@
 /*global $*/
+(function() {
+    // for sliding search bar
+    document.addEventListener("touchstart", function(){}, true);
 
-document.addEventListener("touchstart", function(){}, true);
+    function updateNavbar() {
+        var activeLink = '#' + $('#activeLink').val();
+        $('.navbar-nav').find('li').removeClass('active');
+        $(activeLink).addClass('active');
+    }
 
-function updateNavbar() {
-    var activeLink = '#' + $('#activeLink').val();
-    $('.navbar-nav').find('li').removeClass('active');
-    $(activeLink).addClass('active');
-}
+    function initBigImages() {
+        $('.big-image').abigimage();
+    }
 
-function initBigImages() {
-    $('.big-image').abigimage();
-}
+    $(document).ready(function() {
+        updateNavbar();
+        initBigImages();
+    });
 
-$(document).ready(function() {
-    updateNavbar();
-    initBigImages();
-});
+}());
